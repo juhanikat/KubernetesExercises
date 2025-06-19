656ddcbc35d4b71dbc977ea6b0aa624f07f56fc7
juhanikat <juhani.kataja@gmail.com>
2025-06-19 16:15:31 +0300

    change port number stuff and update deployment

--- chapter2/todo-app/index.js
+++ chapter2/todo-app/index.js
@@ -1,6 +1,6 @@
 import http from "http";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
