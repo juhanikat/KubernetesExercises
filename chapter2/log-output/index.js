d204bb8f1943e8d855012cc7007607c36e946cc3
juhanikat <juhani.kataja@gmail.com>
2025-06-20 16:10:33 +0300

    fiexd log-output

--- chapter2/log-output/index.js
+++ chapter2/log-output/index.js
@@ -1,10 +1,11 @@
-const generateRandomString = () => {
-    let randomString = ""
-    for (let i = 0; i < 10; i++) {
-        randomString = randomString + Math.floor(Math.random() * 10)
-    }
-    console.log(`${new Date()}:${randomString}`)
-    setTimeout(generateRandomString, 5000)
+let randomString = ""
+for (let i = 0; i < 10; i++) {
+    randomString = randomString + Math.floor(Math.random() * 10)
 }
 
-generateRandomString()
+const logRandomString = () => {
+    console.log(`${new Date()}:${randomString}`)
+    setTimeout(logRandomString, 5000)
+}
+
+logRandomString()
